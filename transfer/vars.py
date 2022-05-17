3819a02ebe0554b86e4df8567fd8a9f7998a6723
Vadim <72247526+v4dikos1@users.noreply.github.com>
2022-05-17 10:42:10 +0700

    Update vars.py

--- transfer/vars.py
+++ transfer/vars.py
@@ -1,4 +1,4 @@
-client_id = '125a2f896fd64009a947d88e3c0da521'
-client_secret = '1620b2ac0dae4d92bfdcd2a5e43fa69c'
+client_id = 'YOUR_CLIENT_ID'
+client_secret = 'YOUR_CLIENT_SECRET'
 redirect_uri = 'http://127.0.0.1:8080/'
-token = 'BQC6wfEGIXY_7_2UIecCsgx6nredOx1A4jkiv8_JN'
+token = 'YOUR_TOKEN'
